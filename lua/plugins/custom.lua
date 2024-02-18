2792308c42373b7c648a2f40a521271ddf2a5dff
-LAN- <laipz8200@outlook.com>
2024-02-18 16:40:12 +0800

    feat(plugins): Change to default colorscheme.

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -1,4 +1,11 @@
 return {
   -- add vim-easy-align
   { "junegunn/vim-easy-align", cmd = "EasyAlign", keys = { { "ga", "<Plug>(EasyAlign)", desc = "EasyAlign" } } },
+  {
+    "iamcco/markdown-preview.nvim",
+    ft = "markdown",
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
 }
